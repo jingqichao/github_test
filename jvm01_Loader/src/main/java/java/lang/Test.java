188cd9ede90cb2a131010402a37a3ec32895fbb0
荆启超 <1209446716@qq.com>
2020-09-27 22:41:55 +0800

    双亲委派机制

---
+++ jvm01_Loader/src/main/java/java/lang/Test.java
@@ -0,0 +1,8 @@
+package java.lang;
+
+public class Test {
+    //java.lang.SecurityException: Prohibited package name: java.lang
+    public static void main(String[] args) {
+        System.out.println("1111111");
+    }
+}
